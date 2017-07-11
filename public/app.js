838aee80e458a5976bd2697ced74b2acc28d77fd
Chris <krizloo@gmail.com>
2017-07-11 10:21:20 -0700

    Implement filters and date sort

--- public/app.js
+++ public/app.js
@@ -6,7 +6,6 @@ angular.module('app', [])
       url: 'http://demo7235469.mockable.io/transactions'
     })
     .then(function(resp) {
-      console.log('Response is:', resp);
       return resp.data;
     });
   };
@@ -17,10 +16,45 @@ angular.module('app', [])
 })
 .controller('mainCtrl', function(factory, $scope) {
   $scope.data = {};
+  $scope.ascendingDate = true;
 
   factory.getAll()
   .then(function(data) {
     $scope.data = data;
+    console.log($scope.data);
   });
 
+  $scope.reverseDateSort = function() {
+    // if ($scope.ascendingDate) {    
+    //   $scope.data.transactionData.transactions.sort(function(a, b) {
+    //     var aa = a.transactionDate.split('-').join('');
+    //     var bb = b.transactionDate.split('-').join('');
+    //     return aa < bb ? -1 : (aa > bb ? 1 : 0);
+    //   });
+    // } else {
+    //   $scope.data.transactionData.transactions.sort(function(a, b) {
+    //     var bb = a.transactionDate.split('-').join('');
+    //     var aa = b.transactionDate.split('-').join('');
+    //     return aa < bb ? -1 : (aa > bb ? 1 : 0);
+    //   });
+    // }
+    // $scope.ascendingDate = !$scope.ascendingDate;
+    $scope.data.transactionData.transactions.reverse();
+  };
+
+})
+.filter('unique', function() {
+  return function(collection, keyname) {
+    var output = [];
+    var keys = [];
+
+    angular.forEach(collection, function(item) {
+      var key = item[keyname];
+      if (keys.indexOf(key) === -1) {
+        keys.push(key);
+        output.push(item);
+      }
+    });
+    return output;
+  };
 });
